a3505b589648f6ed9788369b8bbeef43b02ac887
Maximo Chiatellino <mchiatellino@itba.edu.ar>
2024-09-23 18:25:30 -0300

    TPE-ARQUI with old image

--- fileMaker.sh
+++ fileMaker.sh
@@ -1,6 +1,6 @@
-docker start TPEARQUI 
-docker exec -it TPEARQUI make clean -C/root/Toolchain 
-docker exec -it TPEARQUI make clean -C/root/ 
-docker exec -it TPEARQUI make -C/root/Toolchain 
-docker exec -it TPEARQUI make -C/root/ 
-docker stop TPEARQUI
+docker start so 
+docker exec -it so make clean -C/root/Toolchain 
+docker exec -it so make clean -C/root/ 
+docker exec -it so make -C/root/Toolchain 
+docker exec -it so make -C/root/ 
+docker stop so
--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 if
 [ "$1" = "-d" ];
 then
-  qemu-system-x86_64 -s -S -hda Image/x64BareBonesImage.qcow2 -m 512 -soundhw pcspk -audiodev pa,id=RPDSink
+  qemu-system-x86_64 -s -S -hda Image/x64BareBonesImage.qcow2 -m 512 
 else
-  qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -soundhw pcspk -audiodev pa,id=RPDSink
+  qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512
   fi
