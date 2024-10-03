89767fd5bcc008fb2e61e2f7a4058db2b40a1cfc
MaxiChiate <chiatellinomaximo@gmail.com>
2024-10-03 11:59:36 -0300

    Update schedule and new_process

--- Kernel/process/process_management.c
+++ Kernel/process/process_management.c
@@ -9,13 +9,18 @@
 #define BEGINNIN_CURRENT_PROCESS_ADRESS (stacks + PROCESS_AMOUNT*STACK_SPACE - current_process*STACK_SPACE)
 #define STATE_PUSHED_SIZE 15 //Register pushed amount in pushSate macro
 
+#define GLOBAL_SS       0x0
+#define GLOBAL_RFLAGS   0x202
+#define GLOBAL_CS       0x8
+#define INITIAL_ALIGN   0x0
+
 typedef enum preocess_state {BLOCKED, READY, RUNNING} process_state_t;
 
 extern loadContext(uint64_tstack_segment, uint64_tstack_pointer, );
 
-// TODO: cambiar nombres de campos (rflag -> register_flags, sp->stack_pointer, .)
 typedef struct pcb {
     
+    uint64_t align;
     uint64_t stack_segment;
     uint64_t stack_pointer;
     uint64_t register_flags;
@@ -39,24 +44,41 @@ static void next_process()   {
     current_process == 0 ?  current_amount_process=PROCESS_AMOUNT-1 : --current_process;
 }
 
-static void refresh_pcb(unsigned int current_process)   {
+static void refresh_pcb_from_stackcontext(unsigned int p)   {
 
 // Address of last param in process context:
-    int i= pcbs[current_process].stack_pointer + sizeof(stacks[0][0])*STATE_SAVED + 1;
+    int i= pcbs[p].stack_pointer + sizeof(stacks[0][0])*STATE_SAVED + 1;
     
-        pcbs[current_process].stack_segment = stacks[current_process][i++],
-        pcbs[current_process].stack_pointer = stacks[current_process][i++],
-        pcbs[current_process].register_flags = stacks[current_process][i++],
-        pcbs[current_process].code_segment = stacks[current_process][i++],
-        pcbs[current_process].instruction_pointer = stacks[current_process][i++],
-        pcbs[current_process].process_id = stacks[current_process][i++],
-        pcbs[current_process].state = stacks[current_process][i++]    
+        pcbs[p].align = stacks[p][i++];
+        pcbs[p].stack_segment = stacks[p][i++];
+        pcbs[p].stack_pointer = stacks[p][i++];
+        pcbs[p].register_flags = stacks[p][i++];
+        pcbs[p].code_segment = stacks[p][i++];
+        pcbs[p].instruction_pointer = stacks[p][i++];
+        pcbs[p].process_id = stacks[p][i++];
+        pcbs[p].state = stacks[p][i++];
+
+}
+
+static void refresh_stackcontext_from_pcb(unsigned int p)   {
+
+// Address of last param in process context:
+    int i= pcbs[p].stack_pointer + sizeof(stacks[0][0])*STATE_SAVED + 1;
+    
+        stacks[p][i++]= pcbs[p].align;
+        stacks[p][i++]= pcbs[p].stack_segment;
+        stacks[p][i++]= pcbs[p].stack_pointer;
+        stacks[p][i++]= pcbs[p].register_flags;
+        stacks[p][i++]= pcbs[p].code_segment;
+        stacks[p][i++]= pcbs[p].instruction_pointer;
+        stacks[p][i++]= pcbs[p].process_id;
+        stacks[p][i++]= pcbs[p].state;
 
 }
 
 uint64_t schedule(uint64_t current_stack_pointer) {
 
-    refresh_pcb();
+    refresh_pcb_from_stackcontext(current_process);
 
     da  {
         
@@ -66,8 +88,8 @@ uint64_t schedule(uint64_t current_stack_pointer) {
     return pcbs[current_process].stack_pointer;
 }
 
-//TODO:
-void new_process()  {
+//Listo???
+void new_process(uint64_t function_address)  {
 
     if(current_amount_process == PROCESS_AMOUNT) return;
 
@@ -75,17 +97,19 @@ void new_process()  {
 
     pcb_t new_pcb = {
         
-       stack_segment,
-       stack_pointer = (uint64_t) NEW_STACK_ADRESS,
-       register_flags,
-       code_segment,
-       instruction_pointer,
+        align = INITIAL_ALIGN,
+        stack_segment = GLOBAL_SS,
+        stack_pointer = (uint64_t) NEW_STACK_ADRESS,
+        register_flags = GLOBAL_RFLAGS,
+        code_segment = GLOBAL_CS,
+        instruction_pointer = function_address,
         process_id = process_id_counter++,
         state = READY
     };
-}
 
-uint64_t schedule(uint64_t sp) {
-    next_context();
-    return pcbs[current_process].rsp;
+    int new_process_index = PROCESS_AMOUNT-current_amount_process;
+
+    pcbs[new_process_index] = new_pcb;
+
+    refresh_stackcontext_from_pcb(new_process_index);
 }
