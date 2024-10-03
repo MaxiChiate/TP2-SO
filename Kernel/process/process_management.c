cdb17467ef1c2ce35f04d610bb7c025526247409
MaxiChiate <chiatellinomaximo@gmail.com>
2024-10-03 11:38:15 -0300

    Changing names, better index management and Schedule function

--- Kernel/process/process_management.c
+++ Kernel/process/process_management.c
@@ -7,22 +7,15 @@
 #define PROCESS_AMOUNT 64
 #define NEW_STACK_ADRESS (stacks + PROCESS_AMOUNT*STACK_SPACE - 1 - current_amount_process*STACK_SPACE)
 #define BEGINNIN_CURRENT_PROCESS_ADRESS (stacks + PROCESS_AMOUNT*STACK_SPACE - current_process*STACK_SPACE)
+#define STATE_PUSHED_SIZE 15 //Register pushed amount in pushSate macro
 
 typedef enum preocess_state {BLOCKED, READY, RUNNING} process_state_t;
 
-extern loadContext(uint64_t ss, uint64_t rsp, );
+extern loadContext(uint64_tstack_segment, uint64_tstack_pointer, );
 
 // TODO: cambiar nombres de campos (rflag -> register_flags, sp->stack_pointer, .)
 typedef struct pcb {
     
-    uint64_t ss;
-    uint64_t rsp;
-    uint64_t rflags;
-    uint64_t cs;
-    uint64_t rip;
-    uint64_t pid;
-    process_state_t state;
-/*
     uint64_t stack_segment;
     uint64_t stack_pointer;
     uint64_t register_flags;
@@ -30,26 +23,47 @@ typedef struct pcb {
     uint64_t instruction_pointer;
     uint64_t process_id;
     process_state_t state;
-*/
 
 } pcb_t;
 
 static pcb_t pcbs [PROCESS_AMOUNT];
 static uint64_t stacks [PROCESS_AMOUNT][STACK_SPACE];
 
-static unsigned int current_process = 0;
+static unsigned int current_process = PROCESS_AMOUNT-1; // Stacks grow backward
 static unsigned int current_amount_process = 0;
-static unsigned int pid_counter = 1;
+static unsigned int process_id_counter = 1;
 
 
 static void next_process()   {
     
-    current_process = (++current_process) % PROCESS_AMOUNT;
+    current_process == 0 ?  current_amount_process=PROCESS_AMOUNT-1 : --current_process;
 }
 
-void next_context() {
+static void refresh_pcb(unsigned int current_process)   {
 
-    next_process();
+// Address of last param in process context:
+    int i= pcbs[current_process].stack_pointer + sizeof(stacks[0][0])*STATE_SAVED + 1;
+    
+        pcbs[current_process].stack_segment = stacks[current_process][i++],
+        pcbs[current_process].stack_pointer = stacks[current_process][i++],
+        pcbs[current_process].register_flags = stacks[current_process][i++],
+        pcbs[current_process].code_segment = stacks[current_process][i++],
+        pcbs[current_process].instruction_pointer = stacks[current_process][i++],
+        pcbs[current_process].process_id = stacks[current_process][i++],
+        pcbs[current_process].state = stacks[current_process][i++]    
+
+}
+
+uint64_t schedule(uint64_t current_stack_pointer) {
+
+    refresh_pcb();
+
+    da  {
+        
+        next_process();
+    }   while(pcbs[current_process].state != READY);
+    
+    return pcbs[current_process].stack_pointer;
 }
 
 //TODO:
@@ -61,12 +75,12 @@ void new_process()  {
 
     pcb_t new_pcb = {
         
-        ss,
-        rsp = (uint64_t) NEW_STACK_ADRESS,
-        rflags,
-        cs,
-        rip,
-        pid = pid_counter++,
+       stack_segment,
+       stack_pointer = (uint64_t) NEW_STACK_ADRESS,
+       register_flags,
+       code_segment,
+       instruction_pointer,
+        process_id = process_id_counter++,
         state = READY
     };
 }
