ad3fffebb40521940b44de625031c5d0928d9c29
MatiasColeur <mcoleur@itba.edu.ar>
2024-11-11 20:09:04 -0300

    continue developing phylo

--- Userland/SampleCodeModule/apps/phylo.c
+++ Userland/SampleCodeModule/apps/phylo.c
@@ -7,7 +7,8 @@
 #define EATING  1
 #define HUNGRY  2
 #define INIT_PHILOS 5
-#define MAX_TIME 5000 //miliseconds
+#define TIME_EAT 3000 //miliseconds
+#define TIME_THINK 500 //miliseconds
 #define PHILOS_BLOCK 5
 
 static uint32_t N = 0;
@@ -29,13 +30,41 @@ int8_t mutex;
 int8_t update_mutex;
 
 static void think(int i) {
-    sleep(rand() % MAX_TIME);     // Random time
+    sleep(rand() % TIME_EAT);
 }
 
 static void eat(int i) {
-    sleep(rand() % MAX_TIME);       // Random time
+    sleep(rand() % TIME_THINK);
 }
 
+static int can_eat(int i) {
+
+    return philos[LEFT(i)].state != EATING && philos[RIGHT(i)].state != EATING;
+}
+static void return_forks(int i) {
+    philos[i].state = THINKING; 
+    up(philos[i].left_fork);
+    up(philos[i].right_fork);
+}
+static void take_forks(int i) {
+    philos[i].state = HUNGRY;
+    if (can_eat(i)) {
+        philos[i].state = EATING;
+        down(philos[i].left_fork);
+        down(philos[i].right_fork);
+    }
+}
+static void show_state() {
+    for (int i = 0; i < N; i++) {
+        if (philos[i].state == EATING) { 
+            putChar('E');
+        } else {
+            putChar('.');
+        }
+        putChar(' ');
+    }
+    print("\n");
+}
 
 static void add_philosopher(){
     down(update_mutex);
@@ -47,10 +76,9 @@ static void add_philosopher(){
         philos[N].state = THINKING;
             
         if(N>0){
-            down(mutex);
-                philos[N].left_fork= philos[LEFT(N)].left_fork;
-                philos[N].right_fork= new_sem(1);
-                philos[RIGHT(N)].right_fork = philos[N].right_fork;
+            philos[N].left_fork= new_sem(1);
+            philos[N].right_fork= new_sem(1);
+            philos[RIGHT(N)].right_fork = philos[N].right_fork;
             up(mutex);
         }
         else{
@@ -70,6 +98,21 @@ static void add_philosopher(){
 
 void philosopher(int argc, char ** argv) {
 
+    int i = atoi(argv[0]);
+
+    while (1) {
+        think(i);
+        down(mutex);
+        take_forks(i);
+        up(mutex);
+        if (philos[i].state == EATING) {
+            show_state();
+            eat(i);
+            down(mutex);
+            return_forks(i);
+            up(mutex);
+        }
+    }
 
 }
 
@@ -83,6 +126,11 @@ void phylo(int argc, char ** argv){
     for(int i=0;i<INIT_PHILOS;i++){
         add_philosopher();
     }
+
+    while (1)
+    {
+    }
+    
 
     suicide();
 }
