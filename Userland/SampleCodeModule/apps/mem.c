7ea65a308ee34c07b88455114e5b24c8622c0a84
MatiasColeur <mcoleur@itba.edu.ar>
2024-11-11 22:37:54 -0300

    text message change in mem

--- Userland/SampleCodeModule/apps/mem.c
+++ Userland/SampleCodeModule/apps/mem.c
@@ -3,8 +3,14 @@
 void mem(int argc, char ** argv){
 
     mem_t data = mem_data();
+    print("Total memory:");
     printUlong(data->total_space);
     print("\n");
+    print("Used memory:");
     printUlong(data->used_space);
+    print("\n");
+    print("Free memory:");
+    printUlong(data->total_space - data->used_space);
+
     suicide();
 }
