6f836069841eaa6cb5fa96bdd943c37ad20b7940
MaxiChiate <chiatellinomaximo@gmail.com>
2024-11-13 22:12:40 -0300

    cat working

--- Userland/SampleCodeModule/apps/cat.c
+++ Userland/SampleCodeModule/apps/cat.c
@@ -1,15 +1,44 @@
 #include <Apps/apps.h>
-#include <shell.h>
 
-#define BUFFER_LEN (1<<10)
+#define BUFF_LEN (1<<10)
 
-static char buff[BUFFER_LEN+1];
+static char buff[BUFF_LEN+1];
 
 void cat(int argc, char ** argv){
 
-    read(buff,BUFFER_LEN);
+    int i=0;
+    char c;
 
-    print(buff);
-    
-    suicide();
+    while(true)     {
+
+        c = getChar();
+            
+        if(c == EOF || c == '\0')    {
+
+            buff[i] = '\0';
+            print(buff);
+            suicide();
+        }
+
+        if(c==KEY_BACKSPACE )    {
+
+            if(i!=0)    {
+                
+                int pixelsToDelete = (buff[i-1] == KEY_TAB)? 3 : 1;
+                    
+                for(int j=0; j<pixelsToDelete; ++j) {
+
+                    putChar(KEY_BACKSPACE);
+                }
+                                        
+                buff[--i] = '\0';
+            }   
+        }
+        else if (i < BUFF_LEN - 1)    {
+                        
+            buff[i++] = c;
+            putChar(buff[i-1]);
+        }
+    }
 }
+
